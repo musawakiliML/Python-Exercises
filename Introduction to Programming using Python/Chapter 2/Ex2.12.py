13b5dbbbe8509675a884e6b4f9a2cdf4687c182d
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-18 23:53:47 +0100

    Create Ex2.12.py

---
+++ Introduction to Programming using Python/Chapter 2/Ex2.12.py
@@ -0,0 +1,18 @@
+'''
+12.(Print a table) Write a program that displays the following table:
+
+    a    b    a ** b
+    1    2     1
+    2    3     8
+    3    4     81
+    4    5     1024
+    5    6     15625
+'''
+
+# Displaying the result 
+print("a \t b \t a**b")
+print("1 \t 2 \t 1")
+print("2 \t 3 \t 8")
+print("3 \t 4 \t 81")
+print("4 \t 5 \t 1024")
+print("5 \t 6 \t 15625")
