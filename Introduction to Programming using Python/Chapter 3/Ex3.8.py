379a728106c52b2ebf2cbaabfde156d7a112918c
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-23 00:50:01 +0100

    Update Ex3.8.py

--- Introduction to Programming using Python/Chapter 3/Ex3.8.py
+++ Introduction to Programming using Python/Chapter 3/Ex3.8.py
@@ -4,3 +4,24 @@ to fix the possible loss of accuracy when converting a float value to an int val
 Enter the input as an integer whose last two digits represent the cents. For example, 
 the input 1156 represents 11 dollars and 56 cents.
 '''
+# get user input
+amount = int(input("Enter an amount, for example, 1156: "))
+
+# Calculate the monetary units
+numberOfOneDollars = amount // 100
+amount = amount % 100
+numberOfQuarters = amount // 25
+amount = amount % 25
+numberOfDimes = amount // 10
+amount = amount % 10
+numberOfNickels = amount // 5
+amount = amount % 5
+numberOfPennies = amount
+
+# Displaying the result
+print("Your amount", amount, "consists of\n",
+      "\t", numberOfOneDollars, "dollars\n",
+      "\t", numberOfQuarters, "quarters\n",
+      "\t", numberOfDimes, "dimes\n",
+      "\t", numberOfNickels, "nickels\n",
+      "\t", numberOfPennies, "pennies")
