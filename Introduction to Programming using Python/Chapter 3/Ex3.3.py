5ccc94f01d096e230f8864bc6b99397e644fe8d6
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-22 11:37:27 +0100

    Update Ex3.3.py

--- Introduction to Programming using Python/Chapter 3/Ex3.3.py
+++ Introduction to Programming using Python/Chapter 3/Ex3.3.py
@@ -6,5 +6,37 @@ from www.gps-data-team.com/map/ and compute the estimated area enclosed by these
 Divide the polygon into two triangles and use the for- mula in Programming Exercise 2.14 
 to compute the area of a triangle.)
 '''
+import math
 
+# locations based on latitude and longitude of the states
+x1 = 51.5138505182
+y1 = -0.15690922737098845
+
+x2 = 28.5383355
+y2 = -81.37923649999999
+
+x3 = 32.0835407
+y3 = -81.09983419999998
+
+x4 = 35.2270869
+y4 = -80.84312669999997
+
+# Calculating the sides and area
+side1 = math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
+side2 = math.sqrt(math.pow(x2 - x3, 2) + math.pow(y2 - y3, 2))
+side3 = math.sqrt(math.pow(x1 - x3, 2) + math.pow(y1 - y3, 2))
+
+s = (side1 + side2 + side3) / 2
+area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3))
+
+side1 = math.sqrt(math.pow(x1 - x3, 2) + math.pow(y1 - y2, 2))
+side2 = math.sqrt(math.pow(x4 - x3, 2) + math.pow(y4 - y3, 2))
+side3 = math.sqrt(math.pow(x1 - x4, 2) + math.pow(y1 - y4, 2))
+
+s = (side1 + side2 + side3) / 2
+
+area = math.sqrt(s * (s - side1) * (s - side2) * (s - side3)) + area
+
+# displaying the results
+print(f"The Area Enclosed By 4 Cities {area} km")
 
