5f27f1bc9484f24ec08a76fdc15d5840c6231c26
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-22 22:25:38 +0100

    Create Ex3.5.py

---
+++ Introduction to Programming using Python/Chapter 3/Ex3.5.py
@@ -0,0 +1,9 @@
+'''
+*3.5 (Geometry: area of a regular polygon) A regular polygon is an n-sided polygon in which all sides are of the same length
+and all angles have the same degree (i.e., the polygon is both equilateral and equiangular). 
+The formula for computing the area of a regular polygon is
+             area = (n * s^2) / 4 * tan(pi / n)
+Here, s is the length of a side. 
+Write a program that prompts the user to enter the number of sides and 
+their length of a regular polygon and displays its area.
+'''
