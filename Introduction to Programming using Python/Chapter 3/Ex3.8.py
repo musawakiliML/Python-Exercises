b503bc578afbf0359e51cf434cdc1c30b2ba9bdc
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-23 00:27:51 +0100

    Create Ex3.8.py

---
+++ Introduction to Programming using Python/Chapter 3/Ex3.8.py
@@ -0,0 +1,6 @@
+'''
+*3.8 (Financial application: monetary units) Rewrite Listing 3.4, ComputeChange.py, 
+to fix the possible loss of accuracy when converting a float value to an int value. 
+Enter the input as an integer whose last two digits represent the cents. For example, 
+the input 1156 represents 11 dollars and 56 cents.
+'''
