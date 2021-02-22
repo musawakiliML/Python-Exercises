1a7a617a3e076f8d940c8db000adc124801ad774
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-22 22:26:44 +0100

    Update Ex3.5.py

--- Introduction to Programming using Python/Chapter 3/Ex3.5.py
+++ Introduction to Programming using Python/Chapter 3/Ex3.5.py
@@ -7,3 +7,14 @@ Here, s is the length of a side.
 Write a program that prompts the user to enter the number of sides and 
 their length of a regular polygon and displays its area.
 '''
+import math
+
+# Get User input
+number_of_sides = eval(input("Enter the number of sides:"))
+sides = eval(input("Enter the side:"))
+
+# Calculating the area of the pentagon
+area = (number_of_sides * math.pow(sides, 2)) / 4 * math.tan(math.pi / number_of_sides)
+
+# Displaying the results
+print(f"The Area of the polygon is {area}")
