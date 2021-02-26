3d93d38f889c30c243c5b867e03c2c870fd366ef
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-02-26 10:10:22 +0100

    Update Ex3.9.py

--- Introduction to Programming using Python/Chapter 3/Ex3.9.py
+++ Introduction to Programming using Python/Chapter 3/Ex3.9.py
@@ -7,3 +7,27 @@
         Federal tax withholding rate (e.g., 20%) 
         State tax withholding rate (e.g., 9%)
 '''
+# Get user input for payroll
+employee_name = input("Enter employee's name:")
+number_of_hours = eval(input("Enter number of hours worked in a week:"))
+hourly_pay_rate = eval(input("Enter hourly pay rate:"))
+federal_tax_withholding_rate = eval(input("Enter federal tax withholding rate:"))
+state_tax_withholding_rate = eval(input("Enter state tax withholding rate:"))
+
+# Calculating the payroll
+gross_pay = number_of_hours * hourly_pay_rate
+federal_tax_withholding = 0.2 * gross_pay
+state_tax_withholding = state_tax_withholding_rate * gross_pay
+total = federal_tax_withholding + state_tax_withholding
+net = gross_pay - total
+
+# Displaying Results
+print(f"Employee Name:{employee_name}")
+print(f"Hours Worked:{float(number_of_hours)}")
+print(f"Pay Rate:${float(hourly_pay_rate)}")
+print(f"Gross Pay:${float(gross_pay)}")
+print(f"Deductions:")
+print(f"\tFederal Withholding (20.0%):${federal_tax_withholding}")
+print(f"\tState Withholding (9.0%):${state_tax_withholding}")
+print(f"\tTotal Deducation:${total}")
+print(f"Net Pay:${net}")
