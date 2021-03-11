39ff5183baa4b22ce56448edf074a1cc040c195b
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-11 15:56:45 +0100

    Update Ex4.2.py

---
+++ Introduction to Programming using Python/Chapter 4/Ex4.2.py
@@ -0,0 +1,6 @@
+'''
+*4.2 (Game: add three numbers) The program in Listing 4.1 generates two integers 
+and prompts the user to enter the sum of these two integers. 
+Revise the program to generate three single-digit integers 
+and prompt the user to enter the sum of these three integers.
+'''
