1b997da9f02d17d9ca8a5d164ce2306632b44072
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-11 15:54:15 +0100

    Update Ex4.1.py

--- Introduction to Programming using Python/Chapter 4/Ex4.1.py
+++ Introduction to Programming using Python/Chapter 4/Ex4.1.py
@@ -14,3 +14,25 @@ and displays the result based on the discriminant. If the discriminant is positi
 display two roots. If the discriminant is 0, display one root. 
 Otherwise, display The equa- tion has no real roots. 
 '''
+import math 
+
+# Get user input
+a = eval(input("Enter a:"))
+b = eval(input("Enter b:"))
+c = eval(input("Enter c:"))
+
+# Compute the discriminant and the Roots
+r_1 = (-b + math.sqrt(math.pow(b,2) - 4 * a * c)) / (2 * a)
+r_2 = (-b - math.sqrt(math.pow(b,2) - 4 * a * c)) / (2 * a)
+
+discriminant = math.pow(b,2) - 4 * a * c
+
+# Displaying results using If selections
+if discriminant > 0:
+    print(f"The Equations Has Two Real Roots {r_1} and {r_2}.")
+    
+elif discriminant == 0:
+    print(f"The Equation Has One Root {r_1}.")
+    
+else:
+    print("The Equation Has No Real Roots.")
