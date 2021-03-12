39dda5e6ccf60e99725c54c7e0461e1851dcfb84
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-12 11:53:51 +0100

    Update Ex4.5.py

---
+++ Introduction to Programming using Python/Chapter 4/Ex4.5.py
@@ -0,0 +1,6 @@
+'''
+*4.5 (Find future dates) Write a program that prompts the user to enter an integer 
+for today’s day of the week (Sunday is 0, Monday is 1, ..., and Saturday is 6). 
+Also prompt the user to enter the number of days after today for a future day 
+and display the future day of the week.
+'''
