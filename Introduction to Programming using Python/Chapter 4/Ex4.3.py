0d750c18b4d1403ec812f22566e8ae429af8788b
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-12 11:41:24 +0100

    Update Ex4.3.py

--- Introduction to Programming using Python/Chapter 4/Ex4.3.py
+++ Introduction to Programming using Python/Chapter 4/Ex4.3.py
@@ -7,3 +7,14 @@
 Write a program that prompts the user to enter a,b,c,d,e, and f and display the result. 
 If ad - bc = 0,report that The Equation has no solution.
 '''
+# Get User Input
+a, b, c, d, e, f = eval(input("Enter a, b, c, d, e, f:"))
+
+# Calculating the values of x and y
+x = (e * d - b * f) / (a * d - b * c)
+y = (a * f - e * c) / (a * d - b * c)
+
+if (a * d - b * c) == 0:
+    print("The Equation Has No Solution.")
+else:
+    print(f"X is {x} and Y is {y}.")
