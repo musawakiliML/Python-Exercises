83c4e2ae2da4455146e4abc27fa898ff313c6494
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-12 09:32:19 +0100

    Update Ex4.2.py

--- Introduction to Programming using Python/Chapter 4/Ex4.2.py
+++ Introduction to Programming using Python/Chapter 4/Ex4.2.py
@@ -4,3 +4,18 @@ and prompts the user to enter the sum of these two integers.
 Revise the program to generate three single-digit integers 
 and prompt the user to enter the sum of these three integers.
 '''
+import random
+
+# get random numbers
+number_1 = random.randint(0,9)
+number_2 = random.randint(0,9)
+number_3 = random.randint(0,9)
+
+# Display question and accept user input
+answer = eval(input(f"What is {number_1} + {number_2} + {number_3}?"))
+
+# compare answer and display result
+if number_1 + number_2 + number_3 == answer:
+    print(f"{number_1} + {number_2} + {number_3} = {answer} is {number_1 + number_2 + number_3 == answer}")
+else:
+    print(f"{number_1} + {number_2} + {number_3} = {answer} is {number_1 + number_2 + number_3 == answer}")
