4878b7977c3ab878d31c949bdc3f36511c078b97
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-12 11:51:27 +0100

    Update Ex4.4.py

--- Introduction to Programming using Python/Chapter 4/Ex4.4.py
+++ Introduction to Programming using Python/Chapter 4/Ex4.4.py
@@ -4,3 +4,18 @@ and prompts the user to enter the sum of these two integers.
 The program then reports true if the answer is correct, false otherwise. 
 The program is similar to Listing 4.1.
 '''
+import random
+
+# generating two random integers
+integer_1 = random.randint(0,100)
+integer_2 = random.randint(0,100)
+
+# Get user Input
+
+get_sum = eval(input(f"What is {integer_1} + {integer_2}?"))
+
+# Displaying result
+if integer_1 + integer_2 == get_sum:
+    print(f"{integer_1} + {integer_2} = {get_sum} is {integer_1 + integer_2 == get_sum}")
+else:
+    print(f"{integer_1} + {integer_2} = {get_sum} is {integer_1 + integer_2 == get_sum}")
