ee5d7cb4d7653e66b45872d3df0352aee801e1c6
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-12 11:36:49 +0100

    Update Ex4.3.py

---
+++ Introduction to Programming using Python/Chapter 4/Ex4.3.py
@@ -0,0 +1,9 @@
+'''
+*4.3 (Algebra: Solve 2 x 2 Linear Eqautions) You can use the Cramer's to solve the following 2 x 2 system of linear equation:
+        
+        ax + by = e      x = (ed - bf) / (ad - bc)
+        cx + dy = f      y = (af - ec) / (ad - bc)
+        
+Write a program that prompts the user to enter a,b,c,d,e, and f and display the result. 
+If ad - bc = 0,report that The Equation has no solution.
+'''
