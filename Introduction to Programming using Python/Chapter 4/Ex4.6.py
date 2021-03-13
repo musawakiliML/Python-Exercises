ccea4724ef475608780be1882f0c773fed66295e
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-03-13 16:23:44 +0100

    Update Ex4.6.py

---
+++ Introduction to Programming using Python/Chapter 4/Ex4.6.py
@@ -0,0 +1,6 @@
+'''
+*4.6 (Health application: BMI ) Revise Listing 4.6, ComputeBMI.py, to let users enter 
+their weight in pounds and their height in feet and inches. 
+For example, if a person is 5 feet and 10 inches, you will enter 5 for feet 
+and 10 for inches. 
+'''
