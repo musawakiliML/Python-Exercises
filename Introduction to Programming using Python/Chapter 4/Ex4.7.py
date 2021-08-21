b45f4e19cad5c67eb779269fdcb36b4334c4f970
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-08-21 01:42:40 +0100

    Update Ex4.7.py

---
+++ Introduction to Programming using Python/Chapter 4/Ex4.7.py
@@ -0,0 +1,7 @@
+'''
+4.7 (Financial application: monetary units) Modify Listing 3.4, ComputeChange.py,
+to display the nonzero denominations only, using singular words for single units
+such as 1 dollar and 1 penny, and plural words for more than one unit such as 2
+dollars and 3 pennies.
+
+'''
