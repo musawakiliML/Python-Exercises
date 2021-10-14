262d10bcd9458155821f978a3fd98d9957b312f1
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-10-14 13:42:25 +0100

    Update Ex4.8.py

--- Introduction to Programming using Python/Chapter 4/Ex4.8.py
+++ Introduction to Programming using Python/Chapter 4/Ex4.8.py
@@ -2,3 +2,23 @@
 *4.8 (Sort three integers) Write a program that prompts the user to enter three integers
 and displays them in increasing order.
 '''
+
+num1 = eval(input("Enter Number 1:"))
+num2 = eval(input("Enter Number 2:"))
+num3 = eval(input("Enter Number 3:"))
+
+if (num1 > num2) and (num1 > num3):
+    if num2 > num3:
+        print(f"{num3}, {num2}, {num1}")
+    else:
+        print(f"{num2}, {num3}, {num1}")
+if (num2 > num1) and (num2 > num3):
+    if num1 > num3:
+        print(f"{num3}, {num1}, {num2}")
+    else:
+        print(f"{num1}, {num3}, {num2}")
+if (num3 > num2) and (num3 > num1):
+    if num2 > num1:
+        print(f"{num1}, {num2}, {num3}")
+    else:
+        print(f"{num2}, {num1}, {num3}")
