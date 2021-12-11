e6cf1adf3fa9b8af8831c48e201bef5d85290c45
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-12-11 22:49:57 +0100

    Update Ex4.10.py

--- Introduction to Programming using Python/Chapter 4/Ex4.10.py
+++ Introduction to Programming using Python/Chapter 4/Ex4.10.py
@@ -3,3 +3,15 @@
 a subtraction question. Revise the program to randomly generate a multiplication
 question with two integers less than 100.
 '''
+import random
+
+num_1 = random.randint(0,100)
+num_2 = random.randint(0,100)
+
+
+question = input(f"What is {num_1} * {num_2}?")
+
+if question == num_1 * num_2:
+    print("Correct Answer!")
+else:
+    print("Wrong Answer!")
