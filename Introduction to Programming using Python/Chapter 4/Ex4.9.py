e7ce2feaba7b12e7475b87c537adf22213a5c5f1
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-12-11 22:42:46 +0100

    Update Ex4.9.py

--- Introduction to Programming using Python/Chapter 4/Ex4.9.py
+++ Introduction to Programming using Python/Chapter 4/Ex4.9.py
@@ -3,3 +3,14 @@
 packages. The program prompts the user to enter the weight and price of each
 package and then displays the one with the better price.
 '''
+weight_1, price_1 = eval(input("Enter Weight and Price for Package1:"))
+weight_2, price_2 = eval(input("Enter Weight and Price for Package2:"))
+
+price_per_kilo_1 = price_1 / weight_1
+price_per_kilo_2 = price_2 / weight_2
+if price_per_kilo_1 > price_per_kilo_2:
+    print("Package 1 has the better price.")
+elif price_per_kilo_2 > price_per_kilo_1:
+    print("Package 2 has the better price.")
+else:
+    price("Both Have Good Price.")
