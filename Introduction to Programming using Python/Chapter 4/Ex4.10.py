7d3c7763f65cf373199be0873b7d9b47525be780
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-12-11 22:44:02 +0100

    Update Ex4.10.py

---
+++ Introduction to Programming using Python/Chapter 4/Ex4.10.py
@@ -0,0 +1,5 @@
+'''
+4.10 (Game: multiplication quiz) Listing 4.4, SubtractionQuiz.py, randomly generates
+a subtraction question. Revise the program to randomly generate a multiplication
+question with two integers less than 100.
+'''
