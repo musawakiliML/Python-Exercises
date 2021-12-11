3427398f79c0157705487b2a35b1990a1178b38d
Musa Adamu Wakili <musawakiliml@hotmail.com>
2021-12-11 22:34:02 +0100

    Update Ex4.9.py

--- Introduction to Programming using Python/Chapter 4/Ex4.9.py
+++ Introduction to Programming using Python/Chapter 4/Ex4.9.py
@@ -1,5 +1,5 @@
 '''
-*4.9 (Financial: compare costs) Suppose you shop for rice and find it in two differentsized packages. You would like to write a program to compare the costs of the
+*4.9 (Financial: compare costs) Suppose you shop for rice and find it in two different sized packages. You would like to write a program to compare the costs of the
 packages. The program prompts the user to enter the weight and price of each
 package and then displays the one with the better price.
 '''
